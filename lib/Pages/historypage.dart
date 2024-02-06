08bc185d12bb7c8b7735c17cd319cc4be6082f55
ArikSetyawan <setyawanarik@gmail.com>
2024-02-06 21:56:28 +0700

    add: Pull to refresh in History

--- lib/Pages/historypage.dart
+++ lib/Pages/historypage.dart
@@ -24,42 +24,49 @@ class _HistoryPageState extends State<HistoryPage> {
       appBar: AppBar(
         centerTitle: true,
         title: const Text("History"),
+        surfaceTintColor: Colors.transparent,
       ),
-      body: SafeArea(
-        child: BlocBuilder<HistoryBloc, HistoryState>(
-          builder: (context, state) {
-            if (state is HistoryLoaded) {
-              return Padding(
-                padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
-                child: ListView.builder(
-                  itemCount: state.interactions.length,
-                  itemBuilder: (context, index) {
-                    return HistoryCard(interaction: state.interactions[index]);
-                  },
-                ),
-              );
-            } else if (state is HistoryLoading) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (state is HistoryError) {
-              return Center(
-                child: Column(
-                  children: [
-                    Image.asset("assets/images/underconstruction.jpg"),
-                    const Text("This Feature is unavailable.")
-                  ],
-                ),
-              );
-            } else {
-              return Center(
-                child: Column(
-                  children: [
-                    Image.asset("assets/images/underconstruction.jpg"),
-                    const Text("Opps Something Went Wrong!.")
-                  ],
-                ),
-              );
-            }
-          },
+      body: RefreshIndicator(
+        onRefresh: () async {
+          context.read<HistoryBloc>().add(LoadHistory());
+          return;
+        },
+        child: SafeArea(
+          child: BlocBuilder<HistoryBloc, HistoryState>(
+            builder: (context, state) {
+              if (state is HistoryLoaded) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+                  child: ListView.builder(
+                    itemCount: state.interactions.length,
+                    itemBuilder: (context, index) {
+                      return HistoryCard(interaction: state.interactions[index]);
+                    },
+                  ),
+                );
+              } else if (state is HistoryLoading) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (state is HistoryError) {
+                return Center(
+                  child: Column(
+                    children: [
+                      Image.asset("assets/images/underconstruction.jpg"),
+                      const Text("This Feature is unavailable.")
+                    ],
+                  ),
+                );
+              } else {
+                return Center(
+                  child: Column(
+                    children: [
+                      Image.asset("assets/images/underconstruction.jpg"),
+                      const Text("Opps Something Went Wrong!.")
+                    ],
+                  ),
+                );
+              }
+            },
+          ),
         ),
       ),
     );
